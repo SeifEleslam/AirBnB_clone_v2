f1d14a332cd7a0f34c7eba8c234396043ba5f47a
SeifEleslam <seifgouda98@gmail.com>
2024-02-20 01:03:59 +0200

    updated create method

---
+++ tests/test_console.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Console Test"""
+
+from io import StringIO
+import unittest
+
+from unittest.mock import patch
+from console import HBNBCommand
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+
+
+class TestConsole(unittest.TestCase):
+    """TestConsole"""
+
+    def test_create_cmd(self):
+        """test create command"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("create BaseModel"))
+            model = storage.all()["BaseModel."+f.getvalue().strip()]
+            self.assertIsNotNone(model)
+            self.assertIsInstance(model, BaseModel)
+        arg_name = 'sad'
+        arg_val = 'bad'
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd(
+                "create BaseModel {}={}".format(arg_name, arg_val)))
+            model = storage.all()["BaseModel."+f.getvalue().strip()]
+            self.assertIsNotNone(model)
+            self.assertIsInstance(model, BaseModel)
+            self.assertEqual(getattr(model, arg_name), arg_val)
+        output = "** class doesn't exist **"
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("create BaseMode"))
+            self.assertEqual(f.getvalue().strip(), output)
+        output = "** class name missing **"
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("create"))
+            self.assertEqual(f.getvalue().strip(), output)
