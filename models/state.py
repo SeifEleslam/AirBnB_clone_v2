578bb7f33cec333109ef6ae65d6f25ac8fc69f44
SeifEleslam <seifgouda98@gmail.com>
2024-02-21 20:07:10 +0200

    task9

--- models/state.py
+++ models/state.py
@@ -3,12 +3,26 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-# from models.city import City
+
+import os
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship(
-        "City", backref="state", cascade="all, delete")
+    if 'HBNB_TYPE_STORAGE' in os.environ and\
+            os.environ['HBNB_TYPE_STORAGE'] == 'db':
+        cities = relationship(
+            "City", backref="state", cascade="all, delete")
+    else:
+        @property
+        def cities(self):
+            """Return Cities linked to this state"""
+            from models import storage
+            from models.city import City
+            result = []
+            for _, city in storage.all(City).items():
+                if city.state_id == self.id:
+                    result.append(city)
+            return (result)
