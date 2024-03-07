2b182f37cbdd05e2bc6389108f47c773a2db10a9
SeifEleslam <seifgouda98@gmail.com>
2024-03-07 23:12:13 +0200

    task1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Archive Web Static"""
+from fabric.operations import run, sudo, local, put
+from datetime import datetime
+
+
+def do_pack():
+    """Pack  the static files into a tgz."""
+    now = datetime.now()
+    archive_name = f"web_static_{now.year}{now.month:02d}"
+    archive_name += f"{now.day:02d}{now.hour:02d}{now.minute:02d}{now.second:02d}"
+    local("mkdir -p versions")
+    local(f"tar -czvf versions/{archive_name}.tgz web_static")
