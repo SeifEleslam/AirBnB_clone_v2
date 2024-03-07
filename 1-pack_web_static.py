0f05f4a9bcec3d432e67f6ed7ff5c9cde71411f5
SeifEleslam <seifgouda98@gmail.com>
2024-03-07 23:13:25 +0200

    task1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,6 +8,7 @@ def do_pack():
     """Pack  the static files into a tgz."""
     now = datetime.now()
     archive_name = f"web_static_{now.year}{now.month:02d}"
-    archive_name += f"{now.day:02d}{now.hour:02d}{now.minute:02d}{now.second:02d}"
+    archive_name += f"{now.day:02d}{now.hour:02d}"
+    archive_name += f"{now.minute:02d}{now.second:02d}"
     local("mkdir -p versions")
     local(f"tar -czvf versions/{archive_name}.tgz web_static")
