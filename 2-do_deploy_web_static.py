c20ac3787c20a48169ba858e1e0fdca0e8e2cf92
SeifEleslam <seifgouda98@gmail.com>
2024-03-08 14:05:36 +0200

    test

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,26 +3,28 @@
 
 from fabric.api import env, put, run
 
-env.hosts = ['100.26.160.239', '54.158.197.47', '0.0.0.0']
+env.hosts = ['100.26.160.239', '54.158.197.47']
 
 
 def do_deploy(archive_path: str):
     """Pack  the static files into a tgz."""
 
-    try:
-        put(archive_path, f'/tmp', True)
-        file = archive_path.split("/")[-1]
-        name = file.split(".")[0]
-        run(f'rm -rf /data/web_static/releases/{name}')
-        run(f"mkdir -p /data/web_static/releases/{name}")
-        run(f"tar -xzf /tmp/{file} -C /data/web_static/releases/{name}")
-        run(f'rm -rf /tmp/{file}')
-        run(f"mv -f /data/web_static/releases/{name}/web_static/*\
-            /data/web_static/releases/{name}")
-        run(f'rm -rf /data/web_static/releases/{name}/web_static')
-        run('rm -rf /data/web_static/current')
+    # try:
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+    return (
+        put(archive_path, f'/tmp', True).failed is not True and
+        run(f'rm -rf /data/web_static/releases/{name}').failed is not True and
+        run(f"mkdir -p /data/web_static/releases/{name}").failed is not True and
+        run(f"tar -xzf /tmp/{file} -C /data/web_static/releases/{name}").failed is not True and
+        run(f'rm -rf /tmp/{file}').failed is not True and
+        run(f"mv -f /data/web_static/releases/{name}/web_static/* \
+/data/web_static/releases/{name}").failed is not True and
+        run(f'rm -rf /data/web_static/releases/{name}/web_static').failed is not True and
+        run('rm -rf /data/web_static/current').failed is not True and
         run(f'ln -s /data/web_static/releases/{name}/ \
-            /data/web_static/current')
-        return True
-    except Exception:
-        return False
+/data/web_static/current').failed is not True
+    )
+    #     return True
+    # except Exception:
+    #     return False
