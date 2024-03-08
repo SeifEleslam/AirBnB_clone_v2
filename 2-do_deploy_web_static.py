caffb03c4dd7f8b03a96133832654e3ff78fdc82
SeifEleslam <seifgouda98@gmail.com>
2024-03-08 15:14:14 +0200

    task2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 """Archive Web Static"""
 
-from fabric.api import env, put, run, local
+from fabric.api import env, put
 import os.path
 
-env.hosts = ['0.0.0.0', '100.26.160.239', '54.158.197.47']
+env.hosts = ['100.26.160.239', '54.158.197.47']
+
+if len(env.hosts) == 0:
+    from fabric.api import local as run
+else:
+    from fabric.api import run
 
 
 def do_deploy(archive_path: str):
@@ -13,19 +18,17 @@ def do_deploy(archive_path: str):
     try:
         file = archive_path.split("/")[-1]
         name = file.split(".")[0]
-        return (
-            put(archive_path, f'/tmp', True).failed is not True and
-            run(f'rm -rf /data/web_static/releases/{name}').failed is not True and
-            run(f"mkdir -p /data/web_static/releases/{name}").failed is not True and
-            run(f"tar -xzf /tmp/{file} -C /data/web_static/releases/{name}").failed is not True and
-            run(f'rm -rf /tmp/{file}').failed is not True and
-            run(f"mv -f /data/web_static/releases/{name}/web_static/* \
-    /data/web_static/releases/{name}").failed is not True and
-            run(f'rm -rf /data/web_static/releases/{name}/web_static').failed is not True and
-            run('rm -rf /data/web_static/current').failed is not True and
-            run(f'ln -s /data/web_static/releases/{name}/ \
-    /data/web_static/current').failed is not True
-        )
+        put(archive_path, f'/tmp', True)
+        run(f'rm -rf /data/web_static/releases/{name}')
+        run(f"mkdir -p /data/web_static/releases/{name}")
+        run(f"tar -xzf /tmp/{file} -C /data/web_static/releases/{name}")
+        run(f'rm -rf /tmp/{file}')
+        run(f"mv -f /data/web_static/releases/{name}/web_static/* \
+/data/web_static/releases/{name}")
+        run(f'rm -rf /data/web_static/releases/{name}/web_static')
+        run('rm -rf /data/web_static/current')
+        run(f'ln -s /data/web_static/releases/{name}/ \
+/data/web_static/current').failed is not True
     except Exception as e:
         print(e)
         return False
