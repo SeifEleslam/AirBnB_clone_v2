975d4c88f621aa0a26b061f0416f27a0283ac472
SeifEleslam <seifgouda98@gmail.com>
2024-03-08 02:11:50 +0200

    task2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Archive Web Static"""
-from fabric.operations import local, put, run
+
+from fabric.operations import put, run
 from fabric.api import env
+
 env.hosts = ['100.26.160.239', '54.158.197.47']
-env.user = 'ubuntu'
 
 
 def do_deploy(archive_path: str):
@@ -15,12 +16,11 @@ def do_deploy(archive_path: str):
         run(f"mkdir -p /data/web_static/releases/{name}")
         run(f"tar -xzf /tmp/{file} -C /data/web_static/releases/{name}")
         run(f'rm -rf /tmp/{file}')
-        run(f"mv /data/web_static/releases/{name}/web_static/*\
-            /data/web_static/releases/{name}")
+        run(
+            f"mv /data/web_static/releases/{name}/web_static/* /data/web_static/releases/{name}")
         run(f'rm -rf /data/web_static/releases/{name}/web_static')
         run('rm -rf /data/web_static/current')
-        run(f'ln -s /data/web_static/releases/{name}/ \
-            /data/web_static/current')
+        run(f'ln -s /data/web_static/releases/{name}/ /data/web_static/current')
         return True
-    except Exception as e:
+    except Exception:
         return False
