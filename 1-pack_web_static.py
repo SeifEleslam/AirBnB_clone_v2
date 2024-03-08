9965c58ecf66e88b15d0098e360b1a787ef5d5ad
SeifEleslam <seifgouda98@gmail.com>
2024-03-08 02:34:50 +0200

    test

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,10 +6,13 @@ from datetime import datetime
 
 def do_pack():
     """Pack  the static files into a tgz."""
-    now = datetime.now()
-    archive_name = f"web_static_{now.year}{now.month:02d}"
-    archive_name += f"{now.day:02d}{now.hour:02d}"
-    archive_name += f"{now.minute:02d}{now.second:02d}"
-    local("mkdir -p versions")
-    local(f"tar -czvf versions/{archive_name}.tgz web_static")
-    return f"versions/{archive_name}.tgz"
+    try:
+        now = datetime.now()
+        archive_name = f"web_static_{now.year}{now.month:02d}"
+        archive_name += f"{now.day:02d}{now.hour:02d}"
+        archive_name += f"{now.minute:02d}{now.second:02d}"
+        local("mkdir -p versions")
+        local(f"tar -czvf versions/{archive_name}.tgz web_static")
+        return f"versions/{archive_name}.tgz"
+    except Exception:
+        return None
---
+++ 3-deploy_web_static.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Archive Web Static"""
+
+from datetime import datetime
+from fabric.api import env, put, run, local
+
+env.hosts = ['100.26.160.239', '54.158.197.47']
+
+
+def do_pack():
+    """Pack  the static files into a tgz."""
+    try:
+        now = datetime.now()
+        archive_name = f"web_static_{now.year}{now.month:02d}"
+        archive_name += f"{now.day:02d}{now.hour:02d}"
+        archive_name += f"{now.minute:02d}{now.second:02d}"
+        local("mkdir -p versions")
+        local(f"tar -czvf versions/{archive_name}.tgz web_static")
+        return f"versions/{archive_name}.tgz"
+    except Exception:
+        return None
+
+
+def do_deploy(archive_path: str):
+    """Pack  the static files into a tgz."""
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+    try:
+        put(archive_path, f'/tmp', True)
+        run(f'rm -rf /data/web_static/releases/{name}')
+        run(f"mkdir -p /data/web_static/releases/{name}")
+        run(f"tar -xzf /tmp/{file} -C /data/web_static/releases/{name}")
+        run(f'rm -rf /tmp/{file}')
+        run(f"mv -f /data/web_static/releases/{name}/web_static/*\
+            /data/web_static/releases/{name}")
+        run(f'rm -rf /data/web_static/releases/{name}/web_static')
+        run('rm -rf /data/web_static/current')
+        run(f'ln -s /data/web_static/releases/{name}/ \
+            /data/web_static/current')
+        return True
+    except Exception:
+        return False
+
+
+def deploy():
+    """Deploy the application to production server."""
+    file = do_pack()
+    if file is None:
+        return False
+    return do_deploy(file)
