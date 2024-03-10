29d7e7f5f89065f5460d8aeec86c97d6d7886ba6
SeifEleslam <seifgouda98@gmail.com>
2024-03-10 23:38:03 +0200

    test101

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,55 @@
+file { '/data':
+  ensure => directory,
+  owner  => ubuntu,
+  group  => ubuntu,
+  mode   => '0644',
+}
+-> file { '/data/web_static':
+  ensure => directory,
+  owner  => ubuntu,
+  group  => ubuntu,
+  mode   => '0644',
+}
+-> file { '/data/web_static/releases':
+  ensure => directory,
+  owner  => ubuntu,
+  group  => ubuntu,
+  mode   => '0644',
+}
+-> file { '/data/web_static/releases/test':
+  ensure => directory,
+  owner  => ubuntu,
+  group  => ubuntu,
+  mode   => '0644',
+}
+
+-> file { '/data/web_static/releases/test/index.html':
+  ensure  => file,
+  owner   => ubuntu,
+  group   => ubuntu,
+  mode    => '0644',
+  content => "Hello World\n",
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  owner   => root,
+  group   => root,
+  mode    => '0644',
+  content => '
+  server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /data/web_static/current/;
+    index index.html index.htm;
+
+    location /hbnb_static/ {
+      alias /data/web_static/current/;
+      autoindex off;
+    }
+
+    # ... other your existing server configuration
+  }
+',
+}
