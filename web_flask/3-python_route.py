ab3df7419d975f7c35f1aadcd9f5e81851a57ae5
SeifEleslam <seifgouda98@gmail.com>
2024-03-22 17:09:49 +0200

    2 test

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""2 FLask Web APP"""
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_is_(text: str):
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python/", defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def py_is_(text: str):
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+if __name__ == '__main__':
+    """Start running on http://localhost:5000/"""
+    app.run('0.0.0.0', '5000', True)
