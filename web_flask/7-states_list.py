10d398d761596d0631b3a7c09c24490a98190fc6
SeifEleslam <seifgouda98@gmail.com>
2024-03-23 02:17:06 +0200

    8 test

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""6 FLask Web APP"""
+"""7 FLask Web APP"""
 from models import storage
 from models.state import State
 from flask import Flask, render_template
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route("/states_list")
+@app.route("/states_list", strict_slashes=False)
 def hello_hbnb():
     states = map(lambda itm: itm[1], storage.all(State).items())
     return render_template('7-states_list.html', states=states)
