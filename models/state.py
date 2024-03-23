84c4d22d7b5a6dacdbf6e3802cc3fc00517e074b
SeifEleslam <seifgouda98@gmail.com>
2024-03-23 07:36:18 +0200

    8 test

--- models/state.py
+++ models/state.py
@@ -10,11 +10,13 @@ import os
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
         cities = relationship(
             "City", backref="state", cascade="all, delete")
     else:
+        name = ""
+
         @property
         def cities(self):
             """Return Cities linked to this state"""
