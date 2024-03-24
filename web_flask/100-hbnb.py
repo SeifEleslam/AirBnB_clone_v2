5e937d42909d787ff9557bd25be3d50eff3f081e
SeifEleslam <seifgouda98@gmail.com>
2024-03-24 03:19:34 +0200

    review tasks

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -3,6 +3,8 @@
 from models import storage
 from models.state import State
 from models.amenity import Amenity
+from models.place import Place
+
 from flask import Flask, render_template
 
 
@@ -16,14 +18,16 @@ def teardown_storage(exception):
     storage.close()
 
 
-@app.route("/hbnb_filters")
+@app.route("/hbnb")
 def list_cities_by_states():
     """List all states in the database."""
     states = storage.all(State).values()
     amenities = storage.all(Amenity).values()
+    places = storage.all(Place).values()
+
     return render_template(
-        '10-hbnb_filters.html',
-        states=states, amenities=amenities)
+        '100-hbnb.html',
+        states=states, amenities=amenities, places=places)
 
 
 if __name__ == '__main__':
